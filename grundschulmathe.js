9a2ae05217c63b2b2ca7b2e661f3cc49e0da53e8
Leonie Hein <hein.leonie@gmx.net>
2022-07-24 15:33:52 +0200

    Versucht alles Zusammenzufassen - e_final

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,42 +1,115 @@
-//Aufgabe a
 alert("Grundschulmathe");
-alert("Erstes Level: Addition!")
 
 //globale Variablen
 var punkte = 0;
 //-------------- Ablauf --------------
-levelAddition();
+function add(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mul(a, b){
+    return a * b;
+}
+
+function div(a, b){
+    return a / b;
+}
+
+function createAdditionQuestion(a, b){
+    var aufgabe = {
+        zahl1: a,
+        zahl2: b,
+        frage: `Wieviel ist ${zahl1} + ${zahl2} ?`
+    }
+    
+    return aufgabe; 
+}
+
+function createSubstractionQuestion(a, b){
+    var zahl1, zahl2;
+    if(a > b){
+        zahl1 = a;
+        zahl2 = b;
+    }
+    else{
+        zahl1 = b;
+        zahl2 = a;
+    }
+    var aufgabe = {
+        zahl1,
+        zahl2,
+        frage: `Wieviel ist ${zahl1} - ${zahl2} ?`
+    }
+    
+    return aufgabe; 
+}
+
+function createMultiplicationQuestion(a, b){
+    var aufgabe = {
+        zahl1: a,
+        zahl2: b,
+        frage: `Wieviel ist ${zahl1} * ${zahl2}?`
+    }
+    
+    return aufgabe;
+
+}
+
+function createDivisionQuestion(a, b){
+    var aufgabe = {
+        zahl1: a,
+        zahl2: b,
+        frage: `Wie viel ist ${zahl1 * zahl2} : ${zahl1}?`
+    }
+
+    return aufgabe;
+}
+
+
+levelStart(add, createAdditionQuestion);
 
 //------------------------------Level 1 Addition -------------------------
-function levelAddition(){
-    for(var i = 1; i <= 5; i++){
+function levelStart(fn, question) {
+  for (var i = 1; i <= 5; i++) {
+    var a = Math.floor(Math.random() * 20) + 1;
+    var b = Math.floor(Math.random() * 20) + 1;
+    var quest = question(a, b)
+    let result = prompt(quest.frage);
+    console.log(result);
 
-        var a = Math.floor(Math.random()*20)+1;
-        var b = Math.floor(Math.random()*20)+1;
-        let result = prompt(`Wieviel ist ${a} + ${b}?`);
-
-        //Umwandlung in ganze Zahl
-        result = parseInt(result, 10)
-        if(result === a + b){
-            punkte++;
-            alert("Richtig, deine Punktezahl: "+punkte);
-        }
-        else{
-            alert("Falsch, deine Punktezahl: "+punkte);
-        }
-
+    //Umwandlung in ganze Zahl
+    result = parseInt(result, 10);
+    if (result === fn(quest.zahl1, quest.zahl2)) {
+      punkte++;
+      alert("Richtig, deine Punktezahl: " + punkte);
+    } else {
+      alert("Falsch, deine Punktezahl: " + punkte);
     }
-    if (punkte>=4)
-{
-    levelSubtraktion();
-}
-else{
-    alert("Versuchs nochmal!")
-}
+  }
+  if (punkte >= 16){
+    alert("Du hast es geschafft!");
+  }
+  else if(punkte >= 12){
+    levelStart(div, createDivisionQuestion);
+  }
+  else if(punkte >= 8){
+    levelStart(mul, createMultiplicationQuestion);
+  } 
+  else if(punkte >= 4) {
+    levelStart(sub, createSubstractionQuestion);
+  }  
+  else {
+    alert("Versuchs nochmal!");
+  }
 }
 
+//Vergangener Versuch
 //----------------------------- Level 2 Subtraktion-------------------------------
-function levelSubtraktion(){
+/*function levelSubtraktion(){
         alert("Naechstes Level: Subtraktion!")
 
         for(var i = 1; i <= 5; i++){
@@ -121,4 +194,4 @@ function levelDivision(){
         else{
             alert("Falsch, deine Punktzahl: "+punkte);
         }
-}}
+}}*/
